20ca4f7d5d3f0c92fa186007828b5acb8e6cfdbf
Keith Watford <keith.l.watford@gmail.com>
2021-07-15 13:17:53 -0400

    added a date_posted

--- models/Post.js
+++ models/Post.js
@@ -3,11 +3,43 @@ const sequelize = require('../config/connection');
 
 class Post extends Model {}
 
-
-
-
-
-
-
+Post.init(
+    {
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        location: {
+            type: DataTypes.STRING,
+        },
+        date_posted: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    },
+);
 
 module.exports = Post;
