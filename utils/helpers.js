163dd7634111ffbbc555252cdc994f6481a98070
Keith Watford <keith.l.watford@gmail.com>
2021-07-19 10:02:19 -0400

    just trying to add a posted time helper function. its not working and saying getHours isnt a function. currently commented out and should effect current code.

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,18 +5,18 @@ module.exports = {
         var day = date.getDate();
         return month + '/' + day + '/' + year;
       },
-      
-      format_time: (time) => {
-        var hr = d.getHours();
-        var min = d.getMinutes();
-        if (min < 10) {
-          min = "0" + min;
-        }
-        var ampm = "am";
-        if (hr > 12) {
-          hr -= 12;
-          ampm = "pm";
-        }
-        return hr + ":" + min + ampm;
-      }
+
+      // format_time: (date) => {
+      //   var hr = date.getHours();
+      //   var min = date.getMinutes();
+      //   if (min < 10) {
+      //     min = "0" + min;
+      //   }
+      //   var ampm = "am";
+      //   if (hr > 12) {
+      //     hr -= 12;
+      //     ampm = "pm";
+      //   }
+      //   return hr + ":" + min + ampm;
+      // }
 }
