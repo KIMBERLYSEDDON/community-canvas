c23235ba48198fc3502bab54bc3fe208682adc99
Keith Watford <keith.l.watford@gmail.com>
2021-07-19 09:45:50 -0400

    added a format_time helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,5 +4,19 @@ module.exports = {
         var month = 1 + date.getMonth();
         var day = date.getDate();
         return month + '/' + day + '/' + year;
+      },
+      
+      format_time: (time) => {
+        var hr = d.getHours();
+        var min = d.getMinutes();
+        if (min < 10) {
+          min = "0" + min;
+        }
+        var ampm = "am";
+        if (hr > 12) {
+          hr -= 12;
+          ampm = "pm";
+        }
+        return hr + ":" + min + ampm;
       }
 }
